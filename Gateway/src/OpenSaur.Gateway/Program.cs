c267ecdb40d5b638da2191eccb3b7ac7aaa6195e
Hao Du <kevindu1986@outlook.com>
2026-05-01 19:23:11 +0700

    cxzczx

--- Gateway/src/OpenSaur.Gateway/Program.cs
+++ Gateway/src/OpenSaur.Gateway/Program.cs
@@ -1,6 +1,4 @@
-using System.Net;
 using Microsoft.AspNetCore.HttpOverrides;
-using Yarp.ReverseProxy.Forwarder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,25 +17,6 @@ builder.Services
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
-var forwarder = app.Services.GetRequiredService<IHttpForwarder>();
-
-var coreGateAddress = builder.Configuration["ReverseProxy:Clusters:coregate:Destinations:coregateApp:Address"];
-var zentryAddress = builder.Configuration["ReverseProxy:Clusters:zentry:Destinations:zentryApp:Address"];
-
-var forwarderHttpClient = new HttpMessageInvoker(new SocketsHttpHandler
-{
-    UseProxy = false,
-    AllowAutoRedirect = false,
-    AutomaticDecompression = DecompressionMethods.None,
-    UseCookies = false,
-    EnableMultipleHttp2Connections = true,
-    ConnectTimeout = TimeSpan.FromSeconds(15)
-});
-
-var forwarderRequestConfig = new ForwarderRequestConfig
-{
-    ActivityTimeout = TimeSpan.FromSeconds(100)
-};
 
 app.UseForwardedHeaders();
 
@@ -50,46 +29,6 @@ app.Use(async (context, next) =>
         context.Request.Path.Value);
 
     await next();
-});
-
-app.Use(async (context, next) =>
-{
-    var destinationPrefix = context.Request.Host.Host switch
-    {
-        "coregate.duchihao.com" when !string.IsNullOrWhiteSpace(coreGateAddress) => coreGateAddress,
-        "zentry.duchihao.com" when !string.IsNullOrWhiteSpace(zentryAddress) => zentryAddress,
-        _ => null
-    };
-
-    if (destinationPrefix is null)
-    {
-        await next();
-        return;
-    }
-
-    app.Logger.LogInformation(
-        "Direct-forwarding request. Host: {Host}; Path: {Path}; Destination: {Destination}",
-        context.Request.Host.Value,
-        context.Request.Path.Value,
-        destinationPrefix);
-
-    var error = await forwarder.SendAsync(
-        context,
-        destinationPrefix,
-        forwarderHttpClient,
-        forwarderRequestConfig,
-        HttpTransformer.Default);
-
-    if (error != ForwarderError.None && !context.Response.HasStarted)
-    {
-        app.Logger.LogError(
-            "Direct forwarding failed. Host: {Host}; Path: {Path}; Error: {Error}",
-            context.Request.Host.Value,
-            context.Request.Path.Value,
-            error);
-
-        context.Response.StatusCode = StatusCodes.Status502BadGateway;
-    }
 });
 
 app.MapReverseProxy();
