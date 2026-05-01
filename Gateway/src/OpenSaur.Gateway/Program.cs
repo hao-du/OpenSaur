c4c8da3b0d20be9de6ff1bbd043dd8183d089879
Hao Du <kevindu1986@outlook.com>
2026-05-01 16:23:02 +0700

    dsd

--- Gateway/src/OpenSaur.Gateway/Program.cs
+++ Gateway/src/OpenSaur.Gateway/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedHost |
+        ForwardedHeaders.XForwardedProto;
+    options.KnownIPNetworks.Clear();
+    options.KnownProxies.Clear();
+});
 
 builder.Services
     .AddReverseProxy()
@@ -6,11 +18,14 @@ builder.Services
 
 var app = builder.Build();
 
+app.UseForwardedHeaders();
+
 app.Use(async (context, next) =>
 {
     app.Logger.LogInformation(
-        "Gateway request received. Host: {Host}; Path: {Path}",
+        "Gateway request received. Host: {Host}; X-Forwarded-Host: {ForwardedHost}; Path: {Path}",
         context.Request.Host.Value,
+        context.Request.Headers["X-Forwarded-Host"].ToString(),
         context.Request.Path.Value);
 
     await next();
